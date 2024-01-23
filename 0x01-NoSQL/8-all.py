f5b638ce3eb08c7de201371ea73b849784781241
Abdoul54 <abdelwahed.akhechane@gmail.com>
2024-01-23 10:59:13 -0800

    all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+
+
 def list_all(mongo_collection):
-    """
-    List all documents in a MongoDB collection.
+    """ List all documents in Python """
+    documents = mongo_collection.find()
 
-    Args:
-        mongo_collection: pymongo collection object.
+    if documents.count() == 0:
+        return []
 
-    Returns:
-        A list containing all documents in the collection.
-    """
-    documents = list(mongo_collection.find())
     return documents
-
-
-if __name__ == "__main__":
-    # Example usage
-    from pymongo import MongoClient
-
-    # Connect to MongoDB server
-    client = MongoClient('mongodb://127.0.0.1:27017')
-
-    # Access the desired database and collection
-    db = client.my_db
-    school_collection = db.school
-
-    # Call the list_all function
-    schools = list_all(school_collection)
-
-    # Print the results
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
